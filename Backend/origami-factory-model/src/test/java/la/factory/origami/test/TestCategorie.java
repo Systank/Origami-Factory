b5bfbba2216373e27ecec1fc87e321b33271512e
Unknown <terence.allart@gmail.com>
2018-09-20 14:59:27 +0200

    Test catégorie

--- Backend/origami-factory-model/src/test/java/la/factory/origami/test/TestCategorie.java
+++ Backend/origami-factory-model/src/test/java/la/factory/origami/test/TestCategorie.java
@@ -3,7 +3,10 @@ package la.factory.origami.test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import la.factory.origami.factory.model.Categorie;
+import la.factory.origami.factory.model.Niveau;
+import la.factory.origami.factory.model.Origami;
 import la.factory.origami.factory.repository.IRepoCategorie;
+import la.factory.origami.factory.repository.IRepoOrigami;
 
 
 public class TestCategorie {
@@ -11,20 +14,42 @@ public class TestCategorie {
 	public static void main(String[] args) {
 		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-context.xml");
+		
+		IRepoOrigami daoOrigami = context.getBean(IRepoOrigami.class);
+
+		Origami cocotte = new Origami();
+		cocotte.setActif(true);
+		cocotte.setImageOri("www.google.com");
+		cocotte.setNbFeuille(1);
+		cocotte.setNiveau(Niveau.Niv1);
+		cocotte.setNom("Cocotte en papier");
+		cocotte.setNote((float) 4.5);
+		cocotte.setTempsRea("20 mn");
+		cocotte.setYoutube("www.youtube.com");
+		daoOrigami.save(cocotte);
+		
 		IRepoCategorie daoCategorie = context.getBean(IRepoCategorie.class);
 
 		Categorie animaux = new Categorie();
 		animaux.setNom("Animaux");
+		daoCategorie.save(animaux); 
 		
 		Categorie dragons = new Categorie(); 
 		dragons.setNom("Dragons");
+		dragons.setSuperCat(animaux);
+		daoCategorie.save(dragons);
+
+		
 		
 		Categorie pokemons = new Categorie(); 
 		pokemons.setNom("Pokemons");
-		
-		daoCategorie.save(animaux); 
-		daoCategorie.save(dragons); 
+		pokemons.setSuperCat(animaux);
 		daoCategorie.save(pokemons);
+		
+		animaux.getOrigamis().add(cocotte);
+		
+		 
+	
 
 		
 		context.close();
