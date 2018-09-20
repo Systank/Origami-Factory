a306542d5d4b28dac0fb2a0c888a9b57d842b798
Unknown <terence.allart@gmail.com>
2018-09-20 12:26:23 +0200

    Modif categorie

--- Backend/origami-factory-model/src/main/java/la/factory/origami/factory/model/Admin.java
+++ Backend/origami-factory-model/src/main/java/la/factory/origami/factory/model/Admin.java
@@ -1,7 +1,17 @@
 package la.factory.origami.factory.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admin")
 public class Admin {
+	@Id
+	@GeneratedValue
 	private Long id; 
+	
 	private String username; 
 	private String password;
 	
--- Backend/origami-factory-model/src/main/java/la/factory/origami/factory/model/Categorie.java
+++ Backend/origami-factory-model/src/main/java/la/factory/origami/factory/model/Categorie.java
@@ -1,18 +1,92 @@
 package la.factory.origami.factory.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonView;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+
+
 
 @Entity
+@Table
 public class Categorie {
+	
 	@Id
-	@GenerateValue
-	@JsonView(Views.ViewCommon.class)
-
-	private Long id;
+	@GeneratedValue
+    private Long id;
+	@Column(name = "nom", length = 50)
+	@NotEmpty(message = "Le nom est obligatoire")
 	private String nom;
+	@Column(name = "Categorie", length = 100)
+	@NotEmpty(message = "La catégorie est obligatoire")
 	private Categorie SuperCat;
+	@ManyToMany(mappedBy = "catégories", fetch = FetchType.LAZY)
 	private List<Origami> origamis = new ArrayList<>();
+	
+	
+	public Categorie() {
+		super();
+	}
+	
 
+
+	public Categorie(String nom, Categorie superCat) {
+		super();
+		this.nom = nom;
+		SuperCat = superCat;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public Categorie getSuperCat() {
+		return SuperCat;
+	}
+
+
+	public void setSuperCat(Categorie superCat) {
+		SuperCat = superCat;
+	}
+
+
+	public List<Origami> getOrigamis() {
+		return origamis;
+	}
+
+
+	public void setOrigamis(List<Origami> origamis) {
+		this.origamis = origamis;
+	}
+
+	
+	
 }
+
+
