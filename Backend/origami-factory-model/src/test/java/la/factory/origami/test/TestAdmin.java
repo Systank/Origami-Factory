11fa38f9449262284b62d89a88381e30d79551c9
Unknown <terence.allart@gmail.com>
2018-09-20 14:34:53 +0200

    test admin

--- Backend/origami-factory-model/src/test/java/la/factory/origami/test/TestAdmin.java
+++ Backend/origami-factory-model/src/test/java/la/factory/origami/test/TestAdmin.java
@@ -1,5 +1,26 @@
 package la.factory.origami.test;
 
-public class TestAdmin {
+import java.text.ParseException;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import la.factory.origami.factory.model.Admin;
+import la.factory.origami.factory.repository.IRepoAdmin;
+
+
+public class TestAdmin {
+	public static void main(String[] args)  {
+		
+	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-context.xml");
+	
+	IRepoAdmin daoAdmin = context.getBean(IRepoAdmin.class);
+	
+	Admin charloulou = new Admin(); 
+	charloulou.setUsername("Charloulou"); 
+	charloulou.setPassword("loulou");
+	
+	daoAdmin.save(charloulou); 
+	
+	context.close();
+}
 }
