d355b8fb2beff19e5bed9685d6879066b9bdc56a
Unknown <terence.allart@gmail.com>
2018-09-20 16:58:44 +0200

    Auto stash before merge of "master" and "origin/master"

--- Backend/origami-factory-model/src/main/java/la/factory/origami/factory/repository/IRepoCategorie.java
+++ Backend/origami-factory-model/src/main/java/la/factory/origami/factory/repository/IRepoCategorie.java
@@ -15,7 +15,7 @@ public interface IRepoCategorie extends JpaRepository<Categorie, Long> {
 
 	@Query("select c from Categorie c left join fetch c.origamis o where c.id = :id")
 	Categorie findByIdWithOrigamis(@Param("id") Long id);
-    @Query()
+    @Query("select distinct c from Categorie c left join fetch c.sousCats s where s.id = :id")
 	Categorie findByIdWithCategories(Long id);
 
 
