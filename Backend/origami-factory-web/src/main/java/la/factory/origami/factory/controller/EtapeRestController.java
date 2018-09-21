a8ddf4621f9201f31347b1119f69ce1b1f3111e1
Systank <lucas.crappeel@laposte.net>
2018-09-21 11:57:34 +0200

    Ajout du CrossOrigin dans Etape

--- Backend/origami-factory-web/src/main/java/la/factory/origami/factory/controller/EtapeRestController.java
+++ Backend/origami-factory-web/src/main/java/la/factory/origami/factory/controller/EtapeRestController.java
@@ -3,6 +3,7 @@ package la.factory.origami.factory.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import la.factory.origami.factory.model.Etape;
 import la.factory.origami.factory.model.Views;
 import la.factory.origami.factory.repository.IRepoEtape;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/etape")
 public class EtapeRestController {
