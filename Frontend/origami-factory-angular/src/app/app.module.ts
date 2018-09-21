f036784902f2848718931a2398b26e84ec0c0d0c
Unknown <terence.allart@gmail.com>
2018-09-21 13:03:11 +0200

    Angular : Catégorie fonctionne

--- Frontend/origami-factory-angular/src/app/app.module.ts
+++ Frontend/origami-factory-angular/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { EtapeComponent } from './etape/etape.component';
-import { CategorieComponent } from './categorie/categorie.component';
+import {AppComponent} from './app.component';
+import {EtapeComponent} from './etape/etape.component';
+import {CategorieComponent} from './categorie/categorie.component';
 import {OrigamiComponent} from './origami/origami.component';
 import {RouterModule, Routes} from '@angular/router';
-import {AppConfigService} from "./app-config.service";
+import {AppConfigService} from './app-config.service';
+import {CategorieModule} from './categorie/categorie.module';
 
-const routes: Routes = [
+const routes: Routes = [];
 
-];
 @NgModule({
   declarations: [
     AppComponent
@@ -23,11 +23,12 @@ const routes: Routes = [
     HttpModule,
     RouterModule,
     RouterModule.forRoot(routes),
-    EtapeComponent,
-    CategorieComponent,
-    OrigamiComponent
+    // EtapeComponent,
+    CategorieModule,
+    // OrigamiModule
   ],
   providers: [AppConfigService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- Frontend/origami-factory-angular/src/app/categorie/categorie.module.ts
+++ Frontend/origami-factory-angular/src/app/categorie/categorie.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CategorieComponent} from './categorie.component';
-import {MatiereDetailComponent} from './matiere-detail.component';
+
 import {RouterModule, Routes} from '@angular/router';
-import {NgModule} from '@angular/core';
+
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {CategorieService} from './categorie.service';
 import {CategorieDetailComponent} from './categorie-detail.component';
-import {RouterModule, Routes} from '@angular/router';
+
 
 
 const routes: Routes = [
