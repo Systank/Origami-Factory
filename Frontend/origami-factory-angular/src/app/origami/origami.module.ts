6a6aefb0701db6637ee2e3fc3479e824c364092a
Unknown <paul.tauzia@soprasteria.com>
2018-09-21 14:37:38 +0200

    recupération origami

--- Frontend/origami-factory-angular/src/app/origami/origami.module.ts
+++ Frontend/origami-factory-angular/src/app/origami/origami.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
+
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
